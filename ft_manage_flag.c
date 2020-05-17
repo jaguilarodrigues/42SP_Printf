3a7a9e6b29831a2ad08094ba7dae1d80e4af7236
Jaqueline R Aguilar <jaguilarodrigues@gmail.com>
2020-05-17 20:57:18 -0300

    ft_replace_star gets agr

--- ft_manage_flag.c
+++ ft_manage_flag.c
@@ -6,7 +6,7 @@
 /*   By: jaqrodri <jaqrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/15 01:31:40 by jaqrodri          #+#    #+#             */
-/*   Updated: 2020/05/17 11:41:30 by jaqrodri         ###   ########.fr       */
+/*   Updated: 2020/05/17 13:28:08 by jaqrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@ int		ft_isspecifier(char c)
 {
 	return (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i'
 	|| c == 'u' || c == 'x' || c == 'X');
+}
+
+void	ft_replace_star(t_params *prms)
+{
+	long int	num;
+	num = (long int)va_arg(prms->ap, void *);
+	num = num * 1;//apagar
+	// printf("/%ld/", num);
 }
 
 int	ft_catch_flag(t_params *prms)
@@ -33,6 +41,11 @@ int	ft_catch_flag(t_params *prms)
 	{
 		if (prms->s[prms->i + i] != '*')
 			prms->flag[i] = prms->s[prms->i + i];
+		else
+		{
+			ft_replace_star(prms);
+		}
+
 		i++;
 	}
 	prms->flag[i] = '\0';
