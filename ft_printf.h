ec38d707866ac24bfe96530795ac11902abe669c
Jaqueline R Aguilar <jaguilarodrigues@gmail.com>
2020-05-18 02:16:38 -0300

    funtion strjoin implemented

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jaqrodri <jaqrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 00:45:13 by jaqrodri          #+#    #+#             */
-/*   Updated: 2020/05/17 23:55:01 by jaqrodri         ###   ########.fr       */
+/*   Updated: 2020/05/18 01:35:31 by jaqrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,16 @@ typedef struct		s_params
 	int				len;
 }					t_params;
 
+int		ft_printf(const char *s, ...);
+void	ft_printf_c(t_params *prms);
+void	ft_printf_d(t_params *prms);
+void	ft_manage_flag(t_params *prms);
+void	ft_check_specifier(t_params *prms);
 int		ft_strlen(char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_itoa(int n);
 void	ft_putnbr(int n);
 void	ft_putstr(char *s);
 void	ft_putchar(char c);
-void	ft_manage_flag(t_params *prms);
-void	ft_check_specifier(t_params *prms);
-int		ft_printf(const char *s, ...);
-void	ft_printf_c(t_params *prms);
-void	ft_printf_d(t_params *prms);
 
 #endif
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,26 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaqrodri <jaqrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/15 00:29:12 by jaqrodri          #+#    #+#             */
-/*   Updated: 2020/05/18 02:12:30 by jaqrodri         ###   ########.fr       */
+/*   Created: 2020/05/18 01:44:59 by jaqrodri          #+#    #+#             */
+/*   Updated: 2020/05/18 02:14:39 by jaqrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		main(void)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	//ft_printf("teste da stdarg.h |%-20.i| \n", 23425);
-	//ft_putstr("abcdef\n");
-	// printf("%d\n", ft_strlen("asdfg"));
+	int		s1_len;
+	int		s2_len;
+	char	*ns;
+	int		i;
+	int		j;
 
-	//printf("|%s|\n", ft_itoa(1234567));
-
-	printf("|%s|\n", ft_strjoin("teste","se junta"));
-	// printf("|%d|\n", ft_strjoin("teste","se junta")[13]);
-	return (0);
+	i = 0;
+	j = 0;
+	s1_len = ft_strlen((char *)s1);
+	s2_len = ft_strlen((char *)s2);
+	if (!(ns = malloc((s1_len + s2_len + 1) * sizeof(char))))
+		return (NULL);
+	while (i < s1_len)
+	{
+		ns[i] = s1[i];
+		i++;
+	}
+	while (j < s2_len)
+	{
+		ns[i + j] = s2[j];
+		j++;
+	}
+	ns[i + j] = '\0';
+	return (ns);
 }
