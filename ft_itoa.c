27695ccf1b66affe5d528402038bcc0bc39ddc3c
Jaqueline R Aguilar <jaguilarodrigues@gmail.com>
2020-05-18 03:55:22 -0300

    fixing itoa that didn't print negative number

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,29 +6,51 @@
 /*   By: jaqrodri <jaqrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/17 23:53:28 by jaqrodri          #+#    #+#             */
-/*   Updated: 2020/05/18 00:52:31 by jaqrodri         ###   ########.fr       */
+/*   Updated: 2020/05/18 03:45:28 by jaqrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int		ft_count_number(int n)
+{
+	int		i;
+	size_t	aux;
+
+	i = 0;
+	if (n < 0)
+	{
+		i++;
+		aux = -n;
+	}
+	else
+		aux = n;
+	while((aux = aux/10) > 0)
+		i++;
+	return (i);
+}
+
 char	*ft_itoa(int n)
 {
 	int		i;
-	int		aux;
+	size_t	nn;
 	char	*strn;
 
-	i = 0;
-	aux = n;
-	while((aux = aux/10) > 0)
-		i++;
+	i = ft_count_number(n);
 	if (!(strn = malloc((i + 1) * sizeof(char))))
 		return NULL;
-	strn[i + 1] = '\0';
-	while (i >= 0)
+	if (n < 0)
 	{
-		strn[i] = (n % 10) + '0';
-		n = n / 10;
+		strn[0] = '-';
+		nn = -n;
+	}
+	else
+		nn = n;
+	strn[i + 1] = '\0';
+	while (i >= 0 && nn > 0)
+	{
+		strn[i] = (nn % 10) + '0';
+		nn = nn / 10;
 		i--;
 	}
 	return (strn);
