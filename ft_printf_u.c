a2193c3fb4bc7e27422ea10746fd903d93f68387
Jaqueline R Aguilar <jaguilarodrigues@gmail.com>
2020-05-20 02:27:02 -0300

    fix type of va_arg in the unsigned specifier

--- ft_printf_u.c
+++ ft_printf_u.c
@@ -6,7 +6,7 @@
 /*   By: jaqrodri <jaqrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/19 01:09:11 by jaqrodri          #+#    #+#             */
-/*   Updated: 2020/05/19 01:09:54 by jaqrodri         ###   ########.fr       */
+/*   Updated: 2020/05/19 16:02:41 by jaqrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_printf_u(t_params *prms)
 {
 	char	*s;
 
-	s = ft_itoa_base((size_t)va_arg(prms->ap, void *), 10, 'a');
+	s = ft_itoa_base(va_arg(prms->ap, unsigned int), 10, 'a');
 	ft_putstr(s);
 	prms->len += ft_strlen(s);
 }
