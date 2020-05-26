cd1a12a7c9a85b0684c078ea63b97686dc110316
Jaqueline R Aguilar <jaguilarodrigues@gmail.com>
2020-05-26 20:37:49 -0300

    testing

--- src/ft_printf_p.c
+++ src/ft_printf_p.c
@@ -6,7 +6,7 @@
 /*   By: jaqrodri <jaqrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/19 00:31:32 by jaqrodri          #+#    #+#             */
-/*   Updated: 2020/05/26 18:04:01 by jaqrodri         ###   ########.fr       */
+/*   Updated: 2020/05/26 20:36:52 by jaqrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,11 @@ void	ft_printf_p(t_params *prms, t_format *fmt)
 	int		slen;
 
 	s = ft_itoa_base((size_t)va_arg(prms->ap, void *), 16, 'a');
-	slen = (*s == '0') ? 3 : ft_strlen(s);
+	slen = ft_strlen(s);
 	if (!(fmt->neg))
 		ft_putnchar(fmt->width - (slen + 2), fmt->space);
-	if (*s == '0')
-		ft_putstr("(nil)", 5);
-	else
-	{
-		ft_putstr("0x", 2);
-		ft_putstr(s, slen);
-	}
+	ft_putstr("0x", 2);
+	ft_putstr(s, slen);
 	if (fmt->neg)
 		ft_putnchar(fmt->width - (slen + 2), fmt->space);
 	prms->len += (fmt->width > (slen + 2)) ? fmt->width : slen + 2;
