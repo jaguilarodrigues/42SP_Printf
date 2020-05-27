02967ac7b8bfb60b8decb1433032da1549735db5
Jaqueline R Aguilar <jaguilarodrigues@gmail.com>
2020-05-27 04:02:46 -0300

    working! in linux haha

--- src/ft_printf_s.c
+++ src/ft_printf_s.c
@@ -6,7 +6,7 @@
 /*   By: jaqrodri <jaqrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/18 03:06:23 by jaqrodri          #+#    #+#             */
-/*   Updated: 2020/05/26 15:34:46 by jaqrodri         ###   ########.fr       */
+/*   Updated: 2020/05/26 20:49:42 by jaqrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	ft_printf_s(t_params *prms, t_format *fmt)
 	(s == NULL) ? s = "(null)" : 0;
 	slen = ft_strlen(s);
 	notspace = (fmt->prec < slen && fmt->prec > -1) ? fmt->prec : slen;
-	(fmt->space == '0') ? fmt->space = ' ' : 0;
+	// (fmt->space == '0') ? fmt->space = ' ' : 0;
 	if (!(fmt->neg))
 		ft_putnchar(fmt->width - notspace, fmt->space);
 	ft_putstr(s, notspace);
